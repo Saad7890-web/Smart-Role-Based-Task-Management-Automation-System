0c9e8d27a1b78f16c8d1862fc89541a1dc49ff0c
Saad7890-web <omysaadislam@gmail.com>
2025-12-03 21:47:40 +0600

    get task by project api added

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -17,7 +17,25 @@ const createTask = async (req, res) => {
     }
 };
 
+const getAllTasksByProjectId = async (req, res) => {
+    try{
+        const result = await db.query(
+        `SELECT t.*, u.username AS assigned_user 
+         FROM tasks t
+         LEFT JOIN users u ON t.assigned_to = u.id
+         WHERE t.project_id = $1 AND t.is_active = true`
+         [req.params.project_id]
+        );
+
+        res.status(200).json({tasks:result.rows});
+    } catch (err){
+        console.error(err);
+        res.status(500).json({message: "Failed to retrieve tasks"});
+    }
+}
+
 module.exports = {
     createTask,
+    getAllTasksByProjectId,
 }
 
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const auth = require('../middleware/auth');
 const allowRoles = require('../middleware/roles');
 
-const { createTask } = require('../controllers/taskController');
+const { createTask, getAllTasksByProjectId } = require('../controllers/taskController');
 
 router.post('/', auth, allowRoles('Admin', 'Manager'), createTask);
+router.post('/', auth, allowRoles('Admin', 'Manager'), getAllTasksByProjectId);
 
 module.exports = router;
